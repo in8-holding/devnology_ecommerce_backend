bcc09e0e4be1d5b6a5d23fcf8aee024acb76acb6
Gabriel Pink <aws@gabriel-rosa.com>
2025-06-20 13:17:31 -0300

    Refactor AppModule to include HttpModule, ProductsController, and product providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+
+// Controller
+import { ProductsController } from './adapters/in/controllers/products.controller';
+
+// Use Case
+import { GetAllProductsUseCase } from './app/use-cases/get-all-products.use-case';
+
+// Providers externos (adapters/out)
+import { BrazilianProvider } from './adapters/out/http/brazilian.provider';
+import { EuropeanProvider } from './adapters/out/http/european.provider';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule],
+  controllers: [ProductsController],
+  providers: [
+    { provide: 'BrazilianProvider', useClass: BrazilianProvider },
+    { provide: 'EuropeanProvider', useClass: EuropeanProvider },
+    GetAllProductsUseCase,
+  ],
 })
 export class AppModule {}
